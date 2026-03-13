5381c707f2a7d6efbb774f90cc60856008635a0e
unknown <williamhootengarrett@gmail.com>
2026-03-13 08:00:33 -0700

    Ammo item pickups with models and sounds

--- Assets/Scripts/Weapon/AmmoItemDrop.cs
+++ Assets/Scripts/Weapon/AmmoItemDrop.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class AmmoItemDrop : MonoBehaviour
@@ -7,15 +8,52 @@ public class AmmoItemDrop : MonoBehaviour
     public int amount = 30;
 
     [SerializeField] private string _playerTag = "Player";
+    [SerializeField] private AudioClip _pickupSoundEffect;
+
+    private AudioSource _audioSource;
+    private MeshRenderer _meshRenderer;
+    private SphereCollider _collider;
+
+    private void OnEnable()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _collider = GetComponent<SphereCollider>();
+
+        // enable components when pulled from pool or spawned
+        _audioSource.enabled = true;
+        _meshRenderer.enabled = true;
+        _collider.enabled = true;
+    }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(_playerTag))
         {
             Debug.Log("Player walked over ammo pickup");
-            other.GetComponent<PlayerCombat>()?.AddWeaponAmmo(firetype, amount);
-            Destroy(gameObject);
+            if (_audioSource != null && _pickupSoundEffect != null)
+            {
+                StartCoroutine(PickupObjectWithAudio(other));
+            }
+            else
+            {
+                other.GetComponent<PlayerCombat>()?.AddWeaponAmmo(firetype, amount);
+                gameObject.SetActive(false);
+                //Destroy(gameObject);
+            }
         }
+    }
+
+    private IEnumerator PickupObjectWithAudio(Collider other)
+    {
+        other.GetComponent<PlayerCombat>()?.AddWeaponAmmo(firetype, amount);
+        _meshRenderer.enabled = false;
+        _collider.enabled = false;
+        _audioSource.clip = _pickupSoundEffect;
+        _audioSource.Play();
+        yield return new WaitUntil(() => _audioSource.time >= _pickupSoundEffect.length);
+        Debug.Log("Pickup sound effect done playing");
+        gameObject.SetActive(false);
     }
 
 }
